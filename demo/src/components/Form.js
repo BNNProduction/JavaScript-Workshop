c6aa21ab8fbbf78734a61a8ba357a3b83f416a5b
BNNProduction <uthai.boonyaba@gmail.com>
2021-04-21 15:37:20 +0700

    work with button

--- demo/src/components/Form.js
+++ demo/src/components/Form.js
@@ -3,18 +3,24 @@ import { Component } from "react";
 class Form extends Component{
     constructor(){
         super();
-        this.state = {data:"TEST"};
+        this.state = {data:"",message: ""};
         this.doKey = this.doKey.bind(this);
+        //this.sayHi = this.sayHi.bind(this);
     }
     doKey(event){
-        this.setState({data: event.target.value})
+        this.setState({data: event.target.value});
+    }
+    sayHi(){
+        this.setState({message: "Hello "+this.state.data});
     }
 
     render(){
         return (
             <div>
                 <input type="text" onChange={this.doKey}  />
-                <p>Hello : {this.state.data}</p>
+                <button onClick={() => this.sayHi() }>Show It !</button>
+                <p>{this.state.data}</p>
+                <p>{this.state.message}</p>
             </div>
 
         );
